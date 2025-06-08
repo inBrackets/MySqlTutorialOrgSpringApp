53741ed6869907f5f57eca40a833e40c1ce377f2
Jason <extremewinds@gmail.com>
2025-06-08 08:27:41 +0200

    Initial commit

---
+++ src/main/java/org/mysqltutorial/mysqltutorialorgspringapp/MySqlTutorialOrgSpringAppApplication.java
@@ -0,0 +1,13 @@
+package org.mysqltutorial.mysqltutorialorgspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlTutorialOrgSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySqlTutorialOrgSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mysqltutorial/mysqltutorialorgspringapp/MySqlTutorialOrgSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mysqltutorial.mysqltutorialorgspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySqlTutorialOrgSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
