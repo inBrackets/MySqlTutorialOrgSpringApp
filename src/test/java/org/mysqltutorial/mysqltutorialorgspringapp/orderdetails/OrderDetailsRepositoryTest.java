e66b06e6746c1e394223678017d26f47e487486d
Jason <extremewinds@gmail.com>
2025-06-09 13:51:20 +0200

    make the test parametrized

--- src/test/java/org/mysqltutorial/mysqltutorialorgspringapp/orderdetails/OrderDetailsRepositoryTest.java
+++ src/test/java/org/mysqltutorial/mysqltutorialorgspringapp/orderdetails/OrderDetailsRepositoryTest.java
@@ -1,11 +1,16 @@
 package org.mysqltutorial.mysqltutorialorgspringapp.orderdetails;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -14,9 +19,21 @@ class OrderDetailsRepositoryTest {
     @Autowired
     private OrderDetailsRepository orderDetailsRepository;
 
-    @Test
-    void findProductNamesByOrderNumber() {
-        long count = orderDetailsRepository.findProductNamesByOrderNumber(10100).size();
-        assertEquals(4, count);
+    @ParameterizedTest
+    @MethodSource("provideArguments")
+    void findProductNamesByOrderNumber(int orderNumber, int expectedCountOfProducts) {
+        long count = orderDetailsRepository.findProductNamesByOrderNumber(orderNumber).size();
+        assertEquals(expectedCountOfProducts, count);
+    }
+
+    private static Stream<Arguments> provideArguments() {
+        return Stream.of(
+                Arguments.of(10100, 4),
+                Arguments.of(10101, 4),
+                Arguments.of(10102, 2),
+                Arguments.of(10103, 16),
+                Arguments.of(10104, 13),
+                Arguments.of(10105, 15)
+        );
     }
 }
