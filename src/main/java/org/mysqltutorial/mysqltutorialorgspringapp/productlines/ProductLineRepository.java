1b0939470593bbc14f467440353495d578c2e52a
Jason <extremewinds@gmail.com>
2025-06-09 17:22:38 +0200

    reformat, fix n+1 problem

--- src/main/java/org/mysqltutorial/mysqltutorialorgspringapp/productlines/ProductLineRepository.java
+++ src/main/java/org/mysqltutorial/mysqltutorialorgspringapp/productlines/ProductLineRepository.java
@@ -3,10 +3,8 @@ package org.mysqltutorial.mysqltutorialorgspringapp.productlines;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
+interface ProductLineRepository extends CrudRepository<ProductLineEntity, Long> {
 
-interface ProductLineRepository extends CrudRepository <ProductLineEntity, Long> {
-
-    @EntityGraph(attributePaths = "products")  // prevent the n+1 problem
+    @EntityGraph(attributePaths = "products") // prevent the n+1 problem
     Iterable<ProductLineEntity> findAll();
 }
