f6169f30dbba70dfcb13f2f4985ab5d4731bd651
Jason <extremewinds@gmail.com>
2025-06-09 18:08:41 +0200

    Add ProductRepositoryTest

--- src/main/java/org/mysqltutorial/mysqltutorialorgspringapp/products/ProductRepository.java
+++ src/main/java/org/mysqltutorial/mysqltutorialorgspringapp/products/ProductRepository.java
@@ -1,8 +1,12 @@
 package org.mysqltutorial.mysqltutorialorgspringapp.products;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface ProductRepository extends CrudRepository<ProductEntity, Long> {
+
+    @EntityGraph(attributePaths = "productLine")  // prevent the n+1 problem
+    Iterable<ProductEntity> findAll();
 }
---
+++ src/test/java/org/mysqltutorial/mysqltutorialorgspringapp/products/ProductRepositoryTest.java
@@ -0,0 +1,23 @@
+package org.mysqltutorial.mysqltutorialorgspringapp.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ProductRepositoryTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void testFindAll() {
+        assertThat(productRepository.findAll())
+                .hasSize(110);
+    }
+}
