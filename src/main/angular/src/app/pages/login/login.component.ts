914e489ef2d9c2cf2b2a2d318ea3d138b216456b
Jason <extremewinds@gmail.com>
2025-06-19 22:47:12 +0200

    started jwt authentication with https://youtu.be/i5bwLDWXnG4?si=5ItJovUMO8fjn2TF&t=1735

--- src/main/angular/src/app/pages/login/login.component.ts
+++ src/main/angular/src/app/pages/login/login.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {NgClass} from '@angular/common';
+import {UserRegister} from '../../model/user.model';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [NgClass],
+  imports: [NgClass, FormsModule],
   templateUrl: './login.component.html',
   standalone: true,
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
   activeForm: string = "Login";
+
+  registerObj: UserRegister = new UserRegister();
   changeView(formName: string) {
     this.activeForm = formName;
   }
---
+++ src/main/angular/src/app/services/user.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {UserRegister} from '../model/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  registerUser(obj: UserRegister) {
+    this.http.post("https://api.freeprojectapi.com/api/UserApp/CreateNewUser", obj)
+  }
+}
