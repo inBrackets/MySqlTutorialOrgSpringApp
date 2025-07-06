8ddf0e7d3cd4083c37941559906a8478db53ba33
Jason <extremewinds@gmail.com>
2025-07-06 22:48:16 +0200

    fix ProductLineMapper

--- src/main/java/org/mysqltutorial/mysqltutorialorgspringapp/productlines/ProductLineMapper.java
+++ src/main/java/org/mysqltutorial/mysqltutorialorgspringapp/productlines/ProductLineMapper.java
@@ -1,9 +1,11 @@
 package org.mysqltutorial.mysqltutorialorgspringapp.productlines;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 interface ProductLineMapper {
 
+    @Mapping(source = "productLine", target = "productLine")
     ProductLineDto toProductLineDto(ProductLineEntity order);
 }
