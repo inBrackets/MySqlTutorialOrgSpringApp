03bab58ade698db81231d1a34a2ac09f14a7e01b
inBrackets <inBrackets@users.noreply.github.com>
2025-07-10 20:07:16 +0000

    deploy: 3ed78e27fccfc87b99f542a458e92ce9c1dc1cf6

--- src/main/java/org/mysqltutorial/mysqltutorialorgspringapp/customers/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package org.mysqltutorial.mysqltutorialorgspringapp.customers;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
-
-}
--- src/main/resources/db/migration/V2__drop_existing_tables.sql
+++
@@ -1,9 +0,0 @@
-/* Drop existing tables  */
-DROP TABLE IF EXISTS productlines;
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS offices;
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS customers;
-DROP TABLE IF EXISTS payments;
-DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS orderdetails;
